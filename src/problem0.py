e099d170b3469b7a6be0769a937e903aafcb662b
wuh3 <wuh3@rose-hulman.edu>
2019-03-19 11:23:25 -0400

    CSSE 120 L1 M2

--- src/problem0.py
+++ src/problem0.py
@@ -9,8 +9,8 @@ These problems illustrate concepts that previous problems have not emphasized:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Haozhe Wu.
+"""  # done. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 import testing_helper
@@ -25,7 +25,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the green doc-string for the:
+# done.  READ the green doc-string for the:
 #   - is_prime
 #   - sum_of_digits
 # functions defined below.  You do NOT need to understand their
@@ -174,7 +174,9 @@ def run_test_problem0a():
 
 
 def problem0a(n):
+
     """
+
     What comes in:  An integer.
     What goes out:
       -- Returns True if the sum of the digits in the given integer
@@ -188,8 +190,13 @@ def problem0a(n):
       -- If the given integer is 246, this function returns False,
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
+    x=sum_of_digits(n)
+    if x%2==1:
+        return True
+    else:
+        return False
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # done. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -261,8 +268,16 @@ def problem0b(n):
       -- If n is 200, the correct answer is 46,
            since there are 46 primes between 2 and 200.
      """
+    count = 0
+    for k in range(2,n+1):
+        x = is_prime(k)
+
+        if x == True:
+            count=count+1
+    return count
+
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # done. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -329,6 +344,11 @@ def problem0c(circle, n, window):
       :type n: int
       :type window: rg.RoseWindow
     """
+
+    circle1=rg.Circle(circle.center,circle.radius)
+    circle1.attach_to(window)
+    circle2=rg.Circle(rg.Point(circle.center.x+2*circle.radius,circle.center.y),circle.radius)
+    circle2.attach_to(window)
     # -------------------------------------------------------------------------
     # TODO: 5. Implement and test this function.
     #          Tests have been written for you (above).
